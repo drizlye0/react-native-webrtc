85fed6845713c6ef920d2e629876b61be23818b1
drizlye0 <danielkastillo03@gmail.com>
2026-06-25 19:47:54 -0500

    add zoom example

---
+++ examples/ZoomExample/App.tsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { mediaDevices, RTCView } from 'react-native-webrtc';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { use, useState } from 'react';
+
+const streamPromise = mediaDevices.getUserMedia({ video: { facingMode: "environment"} });
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const [zoom, setZoom] = useState<number>(0);
+  const stream = use(streamPromise);
+
+  const increaseZoom = () => {
+    setZoom(zoom + 0.5);
+    stream.getTracks().at(0)?._setZoom(zoom);
+  };
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.container}>
+        <Button title="increase zoom" onPress={increaseZoom} />
+        <RTCView streamURL={stream.toURL()} style={{ flex: 1 }} />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
